d79b9cb26f43e622ec2f0c923e200309e6991c33
burninmedia <stephenl@burnin-media.com>
2024-02-07 11:51:26 -0500

    refactor for docker token

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Validate inputs
+# Input validation
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Error: Missing required arguments: image name, version, and directory."
+  echo "Missing required arguments: image name, version, and directory."
   exit 1
 fi
 
@@ -10,24 +10,21 @@ IMAGE_NAME=$1
 VERSION=$2
 DIRECTORY=$3
 DOCKERFILE=${4:-Dockerfile}
-BUILD_ARGS=$5
+BUILD_ARGS=${5:-}
 REGISTRY_URL=${6:-docker.io}
 REGISTRY_USERNAME=$7
 REGISTRY_PASSWORD=$8
+DOCKERHUB_TOKEN=${9:-}  # New optional Docker Hub token
 
-echo "Logging into Docker registry $REGISTRY_URL..."
-echo "$REGISTRY_PASSWORD" | docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" --password-stdin
-
-echo "Building Docker image $REGISTRY_URL/$IMAGE_NAME:$VERSION..."
-# If build_args is non-empty, convert it to Docker build's --build-arg format
-if [ ! -z "$BUILD_ARGS" ]; then
-  IFS=' ' read -r -a args_array <<< "$BUILD_ARGS"
-  for arg in "${args_array[@]}"; do
-    build_args_cmd+="--build-arg $arg "
-  done
+# Decide whether to use DOCKERHUB_TOKEN or REGISTRY_PASSWORD for login
+if [ ! -z "$DOCKERHUB_TOKEN" ]; then
+  echo "Using Docker Hub token for authentication"
+  echo "$DOCKERHUB_TOKEN" | docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" --password-stdin
+else
+  echo "Using registry password for authentication"
+  echo "$REGISTRY_PASSWORD" | docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" --password-stdin
 fi
 
-docker build -t "$REGISTRY_URL/$IMAGE_NAME:$VERSION" $build_args_cmd--file "$DOCKERFILE" "$DIRECTORY"
-
-echo "Pushing Docker image $REGISTRY_URL/$IMAGE_NAME:$VERSION..."
+# Docker build and push
+docker build -t "$REGISTRY_URL/$IMAGE_NAME:$VERSION" --file "$DOCKERFILE" $BUILD_ARGS "$DIRECTORY"
 docker push "$REGISTRY_URL/$IMAGE_NAME:$VERSION"
